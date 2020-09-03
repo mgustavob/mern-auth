b60c2614c780055c0934fa479c0830e8b519ed35
Martin Briceno <martin.briceno@live.com>
2020-09-03 18:07:38 -0400

    [welcome, about, footer] created

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea incidunt repellat quam natus, culpa autem molestiae cum nesciunt nobis dicta aperiam eum fuga non cupiditate at explicabo enim nostrum dolore!</p>
+        </div>
+    )
+}
+
+
+export default About;
